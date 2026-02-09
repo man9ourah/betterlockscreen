0df74e4efb20dadde0f18023ce6c0002e191eae8
man9ourah <man9our.ah@gmail.com>
2026-02-09 17:17:36 +0300

    use symlinks instead of update-alternatives

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ set -x
 sudo ln -s $PWD/system/betterlockscreen@.service /etc/systemd/system/betterlockscreen@.service
 
 # install the shell script for editing lock screen
-sudo update-alternatives --install /usr/local/bin/betterlockscreen betterlockscreen $PWD/betterlockscreen 100 
+sudo ln -s $PWD/betterlockscreen /usr/local/bin/betterlockscreen
 
 # install fake xflock to integrate with xfce power manager
-sudo update-alternatives --install /usr/local/bin/xflock4 xflock4 $PWD/xflock4 100                                                                                                                                                                                                        
+sudo ln -s $PWD/xflock4 /usr/local/bin/xflock4
